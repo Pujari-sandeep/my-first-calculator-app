e9549d7994e75805720c3630520af6c1d8efda80
Sandeep Kumar <skum0066@inc02f55u8md6m.wisor.com>
2026-01-21 13:08:10 +0530

    Initial commit: spring boot calculator project

---
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -0,0 +1,47 @@
+package com.example.calculator.controller;
+
+import com.example.calculator.model.CalculatorRequest;
+import com.example.calculator.service.CalculatorService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/calculator")
+public class CalculatorController {
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @RequestMapping("/add")
+    public double add(@RequestBody CalculatorRequest request){
+        return calculatorService.add(request.getNum1(), request.getNum2());
+    }
+
+    @RequestMapping("/subtract")
+    public double subtract(@RequestBody CalculatorRequest request){
+        return calculatorService.subtract(request.getNum1(), request.getNum2());
+    }
+    @RequestMapping("/multiply")
+    public double multiply(@RequestBody CalculatorRequest request){
+        return calculatorService.multiply(request.getNum1(), request.getNum2());
+    }
+    @RequestMapping("/division")
+    public double division(@RequestBody CalculatorRequest request){
+        return calculatorService.division(request.getNum1(), request.getNum2());
+    }
+    //completed calculator basics
+    //get-fetch last result
+    @GetMapping("/result")
+    public Object getResult(){
+        Double result=calculatorService.getLastResult();
+        return result!=null?result:"no calculation done yet";
+    }
+
+    //delete clear result
+    @DeleteMapping("/clear")
+    public String clear(){
+        calculatorService.clear();
+        return "calculation cleared successfully";
+    }
+}
---
+++ src/main/java/com/example/calculator/service/CalculatorService.java
@@ -0,0 +1,36 @@
+package com.example.calculator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+
+    private Double lastResult;
+
+    public double add(double a,double b){
+         lastResult=a+b;
+        return lastResult;
+    }
+
+    public Double getLastResult() {
+        return lastResult;
+    }
+
+//    public void setLastResult(Double lastResult) {
+//        this.lastResult = lastResult;
+//    }
+    public void clear(){
+        lastResult=null;
+    }
+
+
+    public double subtract(double a, double b){
+        return a-b;
+    }
+    public double multiply(double a,double b){
+        return a*b;
+    }
+    public double division(double a,double b){
+        return a/b;
+    }
+}
